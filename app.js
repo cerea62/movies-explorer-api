8b50e064cabcf379287e2a0f8267530026b19a34
cerea62 <cerea62@yandex.ru>
2024-02-05 16:21:40 +0300

    added errorHandler

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const express = require('express')
+const express = require('express');
+const routerUser = require('./routes/users');
+const routerAuth = require('./routes/auth');
+const routerMovie = require('./routes/movies');
+const errorHandler = require('./middlewares/errorHandler');
 
 mongoose.connect('mongodb://127.0.0.1:27017//bitfilmsdb', {
   useUnifiedTopology: true,
 });
 const app = express();
 
+app.use(routerAuth);
+app.use(routerUser);
+app.use(routerMovie);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('PORT 3000')
